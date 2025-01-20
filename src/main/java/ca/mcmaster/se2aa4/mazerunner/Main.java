b921a7534b9b889792511e7c9bd2f7eb80fa10de
gmcgaughey@live.com <gmcgaughey@live.com>
2025-01-20 14:56:02 -0500

    Switch to using Apache CLI to parse command line arguments, replace irrelevant print statements with calls to logger

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,20 +1,48 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
 
+    
+
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        // Allow all levels of logging severity to be shown in the terminal.
+        Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.ALL);
+
+        // Setup possible command line arguments.
+        Options options = new Options();
+        options.addOption("i", true, "Path to a maze file");
+        CommandLineParser parser = new DefaultParser();
+
+        logger.info("| Starting Maze Runner**");
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            // Get the maze path from the command line arguments.
+            CommandLine cmd = parser.parse(options, args);
+            String maze_path = cmd.getOptionValue("i");
+
+            // Make sure there is a maze provided. If there isn't one, end the program.
+            if (maze_path == null) {
+                logger.error(" | No path to maze file provided (-i). Terminating program.");
+                System.exit(1);
+            }
+
+            logger.trace("| Reading the maze from file " + maze_path);
+
+            BufferedReader reader = new BufferedReader(new FileReader(maze_path));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
@@ -27,10 +55,10 @@ public class Main {
                 System.out.print(System.lineSeparator());
             }
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.error("/!\\ An error has occured /!\\");
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+        logger.info("| Computing path");
+        logger.warn("| PATH NOT COMPUTED");
+        logger.info("| End of MazeRunner");
     }
 }
