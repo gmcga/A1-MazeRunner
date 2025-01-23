d7a7239f283628d5b333350429c05b3b58b7cebf
gmcgaughey@live.com <gmcgaughey@live.com>
2025-01-23 15:08:19 -0500

    Implement finding path through straight-line maze (close #6

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeExplorer.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeExplorer.java
@@ -1,12 +1,27 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MazeExplorer {
 
+    private static final Logger logger = LogManager.getLogger();
+
     public static String findPathRightHand(char[][] maze, int[] exits, char starting_side) {
-        int[] pos;
-        Direction direction;
-        StringBuilder path = new StringBuilder();
-        path.append("FFFF"); // Using a hardcoded response for now, just to demonstrate functionality.
+        // Assume we start at the left-side entrace of straight.maz.txt.
+        int[] pos = {2, 0};
+        Direction direction = Direction.EAST;
+        int rows = maze.length;
+        int cols = maze[0].length;
+        StringBuilder path = new StringBuilder(); // This should be refactored to use the Path class, but not important for MVP.
+        
+        logger.trace("| Starting maze exploration");
+        
+        while (pos[1] < cols - 1) {
+            pos[1]++;
+            path.append('F');
+        }
+
         return path.toString();
     }
 
